e208bdc431679ea463447cc6cf2152951ef7fe96
Alexandre Lauret <alexlauret54321@gmail.com>
2025-11-01 14:35:43 +0100

    Optimisation de l'augmentation des données

--- src/augmentation.py
+++ src/augmentation.py
@@ -5,11 +5,27 @@ Signature imposée :
 get_augmentation_transforms(config: dict) -> objet/transform callable (ou None)
 """
 from torchvision import transforms
-from torchvision.datasets import ImageNet
-from torch.utils.data import DataLoader
+from torch.utils.data import Dataset, DataLoader
 import torch
 import yaml
 import os
+
+class AugmentationDataset(Dataset):
+    def __init__(self, data_path, column, transform=None):
+        data = torch.load(data_path)
+        self.images = data[column]
+        self.transform = transform
+        
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, i):
+        image = self.images[i]
+        
+        if self.transform:
+            image = self.transform(image)
+        return image
+    
 
 def get_augmentation_transforms(config: dict):
     """Retourne les transformations d'augmentation. À implémenter."""
@@ -26,10 +42,13 @@ def get_augmentation_transforms(config: dict):
         transformations.append(transforms.ColorJitter(**config["augment"]["color_jitter"]))
 
     augmentation_pipeline = transforms.Compose(transformations)
-    train = torch.load(config["dataset"]["split"]["train"]["chemin"])
-    train_augmente = augmentation_pipeline(train[config["dataset"]["columns"]["image"]])
-    
+
+    train_augmente = AugmentationDataset(data_path=config["dataset"]["split"]["train"]["chemin"], 
+                                         column=config["dataset"]["columns"]["image"], 
+                                         transform=train_augmente)
+
     return train_augmente
+
 
 if __name__ == "__main__":
     script_dir = os.path.dirname(os.path.abspath(__file__))
